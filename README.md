292f25d9e86815e9b716b57bcd5c4a8cfaec2f96
Eli Bishop <eli@launchdarkly.com>
2018-12-05 12:32:22 -0800

    initial implementation of Electron hello world (#1)

---
+++ README.md
@@ -0,0 +1,16 @@
+## LaunchDarkly Sample Electron Application 
+
+We've built a simple Electron application that demonstrates how LaunchDarkly's SDK works. 
+
+### Build instructions 
+
+1. Clone this repository: `git clone https://github.com/launchdarkly/hello-electron.git`
+2. Go into the `hello-electron` directory.
+3. If you want the application to display your own feature flags, instead of our example flags, edit `main.js` to set `launchDarklyEnvironmentId` to the client-side ID for your LaunchDarkly environment (shown in [Account Settings](https://app.launchdarkly.com/settings/projects)). (Note that your flags will only be available if you have checked the "Make this flag available to client-side SDKs" box in the feature flag settings.)
+4. Install dependencies: `npm install`
+5. Start the application: `npm start`
+
+### More information
+
+- [Electron SDK Reference](https://dash.readme.io/project/launchdarkly/v2.0/docs/electron-sdk-reference)
+- Github project for the [LaunchDarkly SDKs for Client-Side JavaScript](https://github.com/launchdarkly/js-client), which the Electron SDK is part of
---
+++ main.js
@@ -0,0 +1,58 @@
+//
+// LaunchDarkly Hello World example app for Electron
+//
+// This app requests all the client-side feature flags for a given LaunchDarkly environment
+// and displays them in a window. It uses streaming mode so that if any flag is changed on
+// the LaunchDarkly dashboard, the on-screen value will be updated.
+//
+// The application structure based on the Electron Quick Start Guide: http://electron.atom.io/docs/tutorial/quick-start
+//
+
+const electron = require('electron');
+const ldclient = require('ldclient-electron');
+const path = require('path');
+const url = require('url');
+
+const app = electron.app;
+
+let launchDarklyMainProcessClient;
+
+// Put the client-side ID for your LaunchDarkly environment here. The default value below will
+// still work, but it's for a demo environment where you would not be able to modify the flags.
+const launchDarklyEnvironmentId = '5c0727b6abf23c53a840a9ed';
+
+const launchDarklyUser = {
+  key: 'test-user-key'
+};
+
+const launchDarklyOptions = {
+  streaming: true // Necessary in order for live flag updating to work
+};
+
+function createWindow () {
+  const mainWindow = new electron.BrowserWindow({ width: 800, height: 600 })
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+}
+
+// Wait until Electron says it has fully started up.
+app.on('ready', () => {
+  // Start the LaunchDarkly client in the main process. This is the object that actually
+  // connects to LaunchDarkly; the client object in window.js is just a mirror of it.
+  launchDarklyMainProcessClient = ldclient.initializeInMain(
+    launchDarklyEnvironmentId,
+    launchDarklyUser,
+    launchDarklyOptions
+  );
+
+  createWindow();
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  app.quit();
+});
